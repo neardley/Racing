cacef189552bf239860ab3b49b72de8f05e004bf
Max Tribble <mtribble@trinity.edu>
2021-03-25 15:54:18 -0500

    added speed display

---
+++ Assets/Scripts/UI_Stats.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class UI_Stats : MonoBehaviour
+{
+    TMP_Text speedDisplay;
+
+    [SerializeField] Transform player;
+    Rigidbody playerRB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        speedDisplay = transform.Find("Speed Display").GetComponent<TMP_Text>();
+        playerRB = player.GetComponent<Rigidbody>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //convert meters per sec to mph
+        speedDisplay.text = Math.Round(playerRB.velocity.magnitude* 2.23694).ToString() + " mph";
+    }
+}
