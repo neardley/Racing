57336e3d9e768bb9ff4d3d4e180cd4c6d375c4dd
Max Tribble <mtribble@trinity.edu>
2021-03-25 14:49:31 -0500

    got player movement working

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public float distance = 3.0f;
+    public float height = 3.0f;
+    public float damping = 5.0f;
+
+    void Update()
+    {
+        Vector3 wantedPosition;
+        wantedPosition = target.TransformPoint(0, height, -distance);
+        transform.position = Vector3.Lerp(transform.position, wantedPosition, Time.deltaTime * damping);
+        transform.LookAt(target, target.up);
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,19 +4,46 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    [SerializeField] float topSpeed;
-    [SerializeField] float acceleration;
+    [SerializeField] float maxTorqe;
+    [SerializeField] bool Rearwheeldrive = true;
+    Rigidbody rb;
+
+    WheelCollider FLW, FRW, RLW, RRW;
+
+    float wheelPos, currentTorqe;
+    float maxSteerAngle = 40f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        FLW = transform.Find("Wheel_Front_Left").GetComponent<WheelCollider>();
+        FRW = transform.Find("Wheel_Front_Right").GetComponent<WheelCollider>();
+        RLW = transform.Find("Wheel_Rear_Left").GetComponent<WheelCollider>();
+        RRW = transform.Find("Wheel_Rear_Right").GetComponent<WheelCollider>();
     }
 
-    // Update is called once per frame
-    void Update()
+    // I put all physics related things in here
+    private void FixedUpdate()
     {
+        currentTorqe = Input.GetAxis("Vertical")*maxTorqe;
+        if (Rearwheeldrive)
+        {
+            RLW.motorTorque = currentTorqe;
+            RRW.motorTorque = currentTorqe;
+        }
+        else
+        {
+            FLW.motorTorque = currentTorqe;
+            FRW.motorTorque = currentTorqe;
+        }
+
         
+        wheelPos = Input.GetAxis("Horizontal")*maxSteerAngle;
+        FLW.steerAngle = wheelPos;
+        FRW.steerAngle = wheelPos;
+
+
     }
+
 }
