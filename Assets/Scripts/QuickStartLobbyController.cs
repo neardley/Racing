8a3560da2417135ac266d63ddb591214d9fd521f
NathanEardley <neardley@trinity.edu>
2021-03-27 16:56:36 -0500

    progress?

---
+++ Assets/Scripts/QuickStartLobbyController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class QuickStartLobbyController : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    private GameObject quickStartButton; // button used for creating and joining a game
+    [SerializeField]
+    private GameObject quickCanelButton; // button used to stop searching for a game to join
+    [SerializeField]
+    private int Roomsize; // set the number of players in the room at anyy one time
+    // Start is called before the first frame update
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        quickStartButton.SetActive(true);
+    }
+
+
+    public void QuickStart()
+    {
+        quickStartButton.SetActive(false);
+        quickCanelButton.SetActive(true);
+        PhotonNetwork.JoinRandomRoom();
+    }
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to join a room");
+        CreateRoom();
+    }
+    void CreateRoom()
+    {
+        Debug.Log("Creating room now");
+        int RandomRoomNumber = Random.Range(0, 10000);
+        RoomOptions roomOps = new RoomOptions() 
+            { 
+            IsVisible = true, 
+            IsOpen = true, 
+            MaxPlayers = (byte)Roomsize 
+            };
+        PhotonNetwork.CreateRoom("Room" + RandomRoomNumber, roomOps);
+        Debug.Log(RandomRoomNumber);
+    }
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        Debug.Log("Failed to create room. . . trying again");
+        CreateRoom();
+    }
+
+    public void QuickCanel()
+    {
+        quickCanelButton.SetActive(false);
+        quickStartButton.SetActive(true);
+        PhotonNetwork.LeaveRoom();
+    }
+}
---
+++ Assets/Scripts/QuickStartRoomController.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class QuickStartRoomController : MonoBehaviourPunCallbacks, IInRoomCallbacks
+{
+    //Room Info
+    public static QuickStartRoomController room;
+    private PhotonView PV;
+    public bool isGameLoaded;
+    public int currentScene;
+    public int multiplayScene;
+
+    //Player info
+    Player[] photonPlayers;
+    public int playersInRoom;
+    public int myNumberInRoom;
+    public int playersInGame;
+
+
+    private void Awake()
+    {
+        if(QuickStartRoomController.room == null)
+        {
+            QuickStartRoomController.room = this;
+        } else
+        {
+            if(QuickStartRoomController.room != this)
+            {
+                Destroy(QuickStartRoomController.room.gameObject);
+                QuickStartRoomController.room = this;
+            }
+        }
+        DontDestroyOnLoad(this.gameObject);
+        PV = GetComponent<PhotonView>();
+    }
+
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        PhotonNetwork.AddCallbackTarget(this);
+        SceneManager.sceneLoaded += OnSceneFinishedLoading;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        PhotonNetwork.RemoveCallbackTarget(this);
+        SceneManager.sceneLoaded -= OnSceneFinishedLoading;
+    }
+
+
+    public override void OnJoinedRoom()
+    {
+        base.OnJoinedRoom();
+        Debug.Log("Joined room");
+        photonPlayers = PhotonNetwork.PlayerList;
+        playersInRoom = photonPlayers.Length;
+        PhotonNetwork.NickName = myNumberInRoom.ToString();
+        StartGame();
+    }
+
+    private void StartGame()
+    {
+        if (!PhotonNetwork.IsMasterClient)
+            return;
+        PhotonNetwork.LoadLevel(multiplayScene);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        currentScene = scene.buildIndex;
+        if (currentScene == multiplayScene)
+        {
+            CreatePlayer();
+        }
+    }
+
+    private void CreatePlayer()
+    {
+        Debug.Log("Creating player");
+        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), Vector3.zero, Quaternion.identity, 0);
+    }
+}
